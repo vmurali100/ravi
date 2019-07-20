5dd93236a18cda93d4935ce246e6b6880fe1bc51
vmurali100 <vmurali100@gmail.com>
2019-07-20 13:25:52 +0530

    Adding Cboxes Values

--- sample13_getting_form_values.js
+++ sample13_getting_form_values.js
@@ -9,12 +9,21 @@ function getData() {
   user.lname = document.getElementById("lname").value;
   user.email = document.getElementById("email").value;
 
-  var rbuttons = document.getElementsByName("gender");
-  for (i = 0; i < rbuttons.length; i++) {
+  for (i = 0; i < document.getElementsByName("gender").length; i++) {
     if (rbuttons[i].checked) {
       user.gender = rbuttons[i].value;
     }
   }
 
+  // for Checkboxes
+
+  var cboxes = document.getElementsByName("role");
+  var cboxValue = [];
+  for (i = 0; i < cboxes.length; i++) {
+    if (cboxes[i].checked) {
+      cboxValue.push(cboxes[i].value);
+    }
+  }
+  user.roles = cboxValue;
   console.log(user);
 }
