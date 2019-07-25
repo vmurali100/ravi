5fbfc578beee3ee9ab383d7f53af08e1745751c6
Murali Veesambattu <murali.veesambattu@incedoinc.com>
2019-07-25 10:26:40 +0530

    CRUD Complete

--- sample13_form_Values.js
+++ sample13_form_Values.js
@@ -50,7 +50,7 @@ function displayUsers() {
   document.getElementById("myTable").innerHTML = "";
   for (i = 0; i < users.length; i++) {
     var myTr_ = document.createElement("tr");
-    myTr_.setAttribute("onclick","editUser("+i+")")
+    // myTr_.setAttribute("onclick","editUser("+i+")")
     for (a in users[i]) {
 
       //Capture all the values except roles
@@ -72,19 +72,20 @@ function displayUsers() {
 
     }
 
-    // var editTd = document.createElement("td");
-    // var deleteTd = document.createElement("td");
+    var editTd = document.createElement("td");
+    var deleteTd = document.createElement("td");
 
-    // var editBtn = document.createElement("button");
-    // editBtn.innerHTML = "Edit";
-    // var deleteBtn = document.createElement("button");
-    // deleteBtn.innerHTML = "Delete";
+    var editBtn = document.createElement("button");
+    editBtn.innerHTML = "Edit";
+    editBtn.setAttribute("onclick","editUser("+i+")")
+    var deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Delete";
+    deleteBtn.setAttribute("onclick","deleteUser("+i+")")
+    editTd.appendChild(editBtn);
+    myTr_.appendChild(editTd);
 
-    // editTd.appendChild(editBtn);
-    // myTr_.appendChild(editTd);
-
-    // deleteTd.appendChild(deleteBtn);
-    // myTr_.appendChild(deleteTd);
+    deleteTd.appendChild(deleteBtn);
+    myTr_.appendChild(deleteTd);
 
     document.getElementById("myTable").appendChild(myTr_);
   }
@@ -92,12 +93,44 @@ function displayUsers() {
 
 
 function editUser(i){
-  console.log(users[i])
+  index=i;
+  // console.log(users[i])
   for(a in users[i]){
     document.getElementById(a).value = users[i][a]
   }
 }
 
+function deleteUser(i){
+  users.splice(i,1)
+  displayUsers()
+}
+
 function updateUser(){
-  console.log("updateUser is called")
+  var updatedObj={}
+  updatedObj.fname = document.getElementById("fname").value;
+  updatedObj.lname = document.getElementById("lname").value;
+  updatedObj.email = document.getElementById("email").value;
+
+  var rbuttons = document.getElementsByName("gender");
+  for (i = 0; i < rbuttons.length; i++) {
+    if (rbuttons[i].checked) {
+      updatedObj.gender = rbuttons[i].value;
+    }
+  }
+  
+  var cboxes = document.getElementsByName("role");
+  var cboxValue = [];
+  for (i = 0; i < cboxes.length; i++) {
+    if (cboxes[i].checked) {
+      cboxValue.push(cboxes[i].value);
+    }
+  }
+  updatedObj.roles = cboxValue;
+
+  console.log(updatedObj)
+
+  users[index] = updatedObj;
+  displayUsers()
+  clearForm()
+
 }
