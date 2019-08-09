9fde9c7cf3b165b2e14b8bc2b3f6aaf78d0bcf7c
Murali Veesambattu <murali.veesambattu@incedoinc.com>
2019-08-09 19:01:35 +0530

    Adding Some String and Date Methods

---
+++ CRUD-APP3/data.js
@@ -0,0 +1,26 @@
+var data = [
+    {
+        "fname": "Bernadette",
+        "lname": "Joyner",
+        "tel": "(742)740-2805",
+        "zip": 80436
+    },
+    {
+        "fname": "Marcedia",
+        "lname": "Silva",
+        "tel": "(587)730-1691",
+        "zip": 15104
+    },
+    {
+        "fname": "Guy",
+        "lname": "Pick",
+        "tel": "(279)006-9487",
+        "zip": 39947
+    },
+    {
+        "fname": "Jeri",
+        "lname": "Forgey",
+        "tel": "(851)005-7523",
+        "zip": 79660
+    }
+]
